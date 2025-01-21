f506caf2320a2b9e3f839c5a133c77637ff2333c
Ajay Saravanan <avsoftwaredevelopers@gmail.com>
2025-01-21 11:05:59 +0530

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'firstgit';
+export class AppComponent implements OnInit {
+ data:string="";
+ readme:string="";
+
+ngOnInit(): void {
+
+
+
+
+}
+
+savedata(){
+  if(this.data !=""){
+    let inputdata=this.data;
+    localStorage.setItem("user",inputdata);
+  }
+  else{
+    alert("Please Enter Data");
+  }
+}
+
+readdata(){
+  const read=localStorage.getItem("user");
+  console.log(read);
+
+  if(read !=null){
+    this.readme=read;
+  }
+  alert(this.readme);
+}
+
 }
